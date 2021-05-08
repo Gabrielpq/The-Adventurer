63b071d9161b69cbf8a0c1f0ea5aa52b98a6f716
Gabrielpq <paimaquispegabriel@gmail.com>
2021-05-08 11:49:35 -0500

    Saltary moverte

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody;
+    public float moveSpeed;
+    public float jumpForce;
+    public Vector2 input;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //Moverse
+        input = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, _rigidbody.velocity.y);
+        _rigidbody.velocity = input;
+
+        //Saltar
+        if (Input.GetButtonDown("Jump"))
+            _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, jumpForce);
+    }
+
+}   
