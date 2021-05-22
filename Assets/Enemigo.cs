1cc02e9278095fcf2164a50be6c4e178d69f9161
Gabrielpq <paimaquispegabriel@gmail.com>
2021-05-22 10:41:15 -0500

    Movimiento enemigo

---
+++ Assets/Enemigo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody2D _rigidbody;
+    public int direction = 0;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        FlipSprite();
+    }
+
+    void Update()
+    {
+        _rigidbody.velocity = new Vector2(direction * moveSpeed, _rigidbody.velocity.y);
+    }
+
+    void FlipSprite()
+    {
+        transform.eulerAngles = new Vector3(0, direction == 1 ? 180 : 0, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Pared"))
+        {
+            direction *= -1;
+            FlipSprite();
+        }
+    }
+
+}
